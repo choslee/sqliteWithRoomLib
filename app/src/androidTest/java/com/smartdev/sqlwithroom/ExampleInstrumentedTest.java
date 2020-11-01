4b21c592cf9f361d3b75e16a17a034db329c7dcd
Dragoljub <zivanovic.dragoljub@gmail.com>
2020-11-01 19:38:35 +0100

    Rename project from withoutLib to withRoom ++

--- app/src/androidTest/java/com/smartdev/sqlwithroom/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/smartdev/sqlwithroom/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.smartdev.sqlwithoutlibrary", appContext.getPackageName());
+        assertEquals("com.smartdev.sqlwithRoom", appContext.getPackageName());
     }
 }
